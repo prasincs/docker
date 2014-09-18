c8ef4a7ad2d2196543bb5febe582b1a23af8e69b
Aanand Prasad <aanand.prasad@gmail.com>
2014-09-18 10:55:19 -0700

    Remove currently-unused ports/volumes stuff from GroupConfig

--- runconfig/group.go
+++ runconfig/group.go
@@ -1,25 +1,13 @@
 package runconfig
 
-type GroupVolume struct {
-	Name          string
-	ContainerPath string
-}
-
 type GroupContainer struct {
 	Image   string
 	Command []string
-	Volumes []*GroupVolume
 }
 
 type GroupConfig struct {
-	Name  string
-	Ports struct {
-		HostPort      int
-		ContainerPort int
-		HostIP        string
-	}
-	VolumesRoot string
-	Containers  map[string]*GroupContainer
+	Name       string
+	Containers map[string]*GroupContainer
 }
 
 func (c *GroupContainer) AsRunConfig() *Config {
